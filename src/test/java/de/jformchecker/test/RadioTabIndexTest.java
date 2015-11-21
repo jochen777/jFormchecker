072636525e65bf086dded33ceb7c05c0b2f9d2bd
Jochen Pier <jpier@me.com>
2015-11-21 19:58:02 +0100

    removed unused class.

--- src/test/java/de/jformchecker/test/RadioTabIndexTest.java
+++ src/test/java/de/jformchecker/test/RadioTabIndexTest.java
@@ -8,9 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.junit.Assert;
 
 import de.jformchecker.FormChecker;
-import de.jformchecker.FormCheckerForm;
 import de.jformchecker.elements.RadioInput;
-import de.jformchecker.elements.SelectInput;
 import de.jformchecker.test.builders.ExampleFormBuilder;
 import de.jformchecker.test.builders.RequestBuilders;
 import de.jformchecker.test.builders.SampleMapBuilders;
@@ -19,12 +17,12 @@ public class RadioTabIndexTest {
 
   @org.junit.Test
   public void test() {
-    RadioInput radio = (RadioInput)RadioInput.build("radio")
-        .setPossibleValues(SampleMapBuilders.generateRadioMap())
-        .setDescription("Choice")
+    RadioInput radio = (RadioInput) RadioInput.build("radio")
+        .setPossibleValues(SampleMapBuilders.generateRadioMap()).setDescription("Choice")
         .setTabIndex(4);
     Assert.assertTrue("Expected tabindex=4", radio.getInputTag().contains("tabindex=\"4"));
   }
+
   @org.junit.Test
   public void testForm() {
     Map<String, String> reqVals = new HashMap<>();
@@ -38,21 +36,6 @@ public class RadioTabIndexTest {
     fc.run();
     System.out.println(fc.getCompleteForm());
   }
-  
-  class Form extends FormCheckerForm {
-    @Override
-    public void init() {
 
-      add(RadioInput
-          .build("rdio")
-          .setPossibleValues(SampleMapBuilders.generateRadioMap())
-          .setDescription("Your Choice"));
 
-      add(SelectInput
-          .build("select")
-          .setPossibleValues(SampleMapBuilders.generateSelectMap())
-          .setDescription("Your Selection"));
-
-    }
-  }
 }
