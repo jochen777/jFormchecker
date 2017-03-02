528c670644fe170e23e9b42cea7e706bb0c98815
Jochen Pier <jpier@me.com>
2017-03-02 23:34:48 +0100

    flexible output of tags

--- src/main/java/de/jformchecker/View.java
+++ src/main/java/de/jformchecker/View.java
@@ -2,8 +2,11 @@ package de.jformchecker;
 
 import java.util.Map;
 
+import de.jformchecker.criteria.ValidationResult;
+
 /**
  * Vacade for access to formchecker whitin templates
+ * 
  * @author jochen
  *
  */
@@ -11,7 +14,7 @@ public class View {
 	FormCheckerForm form;
 	GenericFormBuilder formBuilder;
 	FormChecker fc;
-	
+
 	// RFE: try to avoid passing fc
 	View(FormCheckerForm form, GenericFormBuilder formBuilder, FormChecker fc) {
 		this.form = form;
@@ -19,52 +22,79 @@ public class View {
 		this.fc = fc;
 	}
 
+	// this method is useless in most cases. You want to build your
+	// input-element via a macro!
 	public String getElement(String name) {
-		// RFE: Use the FormBuilder Method getCompleteRenderedInput for this. It's more complete
-		StringBuilder element = new StringBuilder(this.getLabel(name));
+		// RFE: Use the FormBuilder Method getCompleteRenderedInput for this.
+		// It's more complete
+
+		StringBuilder element = new StringBuilder();
+		element.append(this.getError(name));
+		element.append(this.getLabel(name));
 		element.append(this.getInput(name));
+		element.append(this.getHelp(name));
 		return element.toString();
 	}
-	
+
 	public String getInput(String name) {
 		return form.getElement(name).getInputTag();
 	}
-	
-	
 
-	public String getLabel(String elementName) {
-		return formBuilder.getLabelForElement(form.getElement(elementName), new TagAttributes(), fc.firstRun);
+	public boolean isError(String name) {
+		return !formBuilder.getErrors(form.getElement(name), fc.firstRun).isValid();
 	}
 
-	public String getLabel(String elementName, Map<String, String> map) {
-		return formBuilder.getLabelForElement(form.getElement(elementName), new TagAttributes(map),
-				fc.firstRun);
+	public String getError(String name) {
+		ValidationResult vr = formBuilder.getErrors(form.getElement(name), fc.firstRun);
+		if (!vr.isValid()) {
+			return fc.config.getProperties().getMessage(vr);
+		}
+		return "";
+	}
+
+	public String getLabel(String name) {
+		if (!StringUtils.isEmpty(form.getElement(name).getDescription())) {
+			return formBuilder.getLabelForElement(form.getElement(name), new TagAttributes(), fc.firstRun);
+		} else {
+			return "";
+		}
+	}
+
+	public String getLabel(String name, Map<String, String> map) {
+		return formBuilder.getLabelForElement(form.getElement(name), new TagAttributes(map), fc.firstRun);
+	}
+	
+	public String getHelp(String name) {
+		FormCheckerElement elem = form.getElement(name);
+		if (!StringUtils.isEmpty(elem.getHelpText())) {
+			return elem.getHelpText();
+		}
+		return "";
 	}
 
 	public String getStart() {
-		StringBuilder formStart = new StringBuilder(formBuilder.generateFormStartTag(form.getId(), fc.formAction, formBuilder.checkMultipart(form.getElements()),
-				formBuilder.createFormTagAttributes(form)));
+		StringBuilder formStart = new StringBuilder(formBuilder.generateFormStartTag(form.getId(), fc.formAction,
+				formBuilder.checkMultipart(form.getElements()), formBuilder.createFormTagAttributes(form)));
 		formStart.append(formBuilder.generateCSRF(fc.req, fc.firstRun, form));
 		return formStart.toString();
 	}
-	
+
 	public String getEnd() {
 		return formBuilder.getEndFormTag();
 	}
-	
+
 	public String getForm() {
 		return formBuilder.generateGenericForm(fc.formAction, fc.firstRun, form, fc.req, fc.config);
 	}
-	
+
 	public String getSubmit(String label) {
-		String t =  formBuilder.getSubmit(label);
+		String t = formBuilder.getSubmit(label);
 		return t;
 	}
 
 	public String getSubmit() {
-		String t =  formBuilder.getSubmit(form.getSubmitLabel());
+		String t = formBuilder.getSubmit(form.getSubmitLabel());
 		return t;
 	}
-
 
 }
