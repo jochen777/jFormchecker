e3c2029fd431a91e34e10272a37a4bd1a6b619ed
Jochen Pier <jpier@me.com>
2019-04-01 12:17:25 +0200

    Add another utility method

--- src/main/java/de/jformchecker/StringUtils.java
+++ src/main/java/de/jformchecker/StringUtils.java
@@ -2,19 +2,33 @@ package de.jformchecker;
 
 /**
  * Some basic string utils to avoid dependance to commons
+ * 
  * @author jochen
  *
  */
 public class StringUtils {
-	
-	public static final String EMPTY_STR = "";
-	
-    public static boolean isEmpty(final CharSequence cs) {
-        return cs == null || cs.length() == 0;
-    }
-    public static String defaultString(final String str) {
-        return str == null ? EMPTY_STR : str;
-    }
 
-    
+  public static final String EMPTY_STR = "";
+
+  public static boolean isEmpty(final CharSequence cs) {
+    return cs == null || cs.length() == 0;
+  }
+
+  public static String defaultString(final String str) {
+    return alternative(str, EMPTY_STR);
+  }
+
+  /**
+   * checks if the original string is empty. if empty/null return the alternative, otherwise the
+   * original
+   * 
+   * @param original
+   * @param alternative
+   * @return
+   */
+  public static String alternative(final String original, final String alternative) {
+    return isEmpty(original) ? alternative : original;
+  }
+
+
 }
